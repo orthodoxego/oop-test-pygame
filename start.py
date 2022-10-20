af6be5530d186ed401bdda6efd68427350a964d6
Cabrikfan <zhenchar@gmail.com>
2022-10-20 17:31:46 +0300

    Светомузыка

--- start.py
+++ start.py
@@ -2,8 +2,10 @@ import pygame
 from setup import *
 from random import randint
 from dataclass.circle import Circle
+from dataclass.square import Square
 
 pygame.init()
+pygame.display.set_caption("Test OOP")
 pygame.display.set_caption("Test OOP")
 size = [WIDTH, HEIGHT]
 scene = pygame.display.set_mode(size)
@@ -12,10 +14,16 @@ playGame = True
 deltatime = 0
 
 crc = []
-for i in range(100):
+for i in range(1):
     crc.append(Circle(randint(0, WIDTH), randint(0, HEIGHT),
                       (randint(0, 255), randint(0, 255), randint(0, 255)),
                       randint(5, 50)))
+
+sqe = []
+for i in range(100):
+    sqe.append(Square(randint(0, WIDTH), randint(0, HEIGHT),
+                      (randint(0, 255), randint(0, 255), randint(0, 255)),
+                      randint(5, 50), randint(0, WIDTH), randint(0, HEIGHT)))
 
 while playGame:
     for event in pygame.event.get():
@@ -31,6 +39,10 @@ while playGame:
     for i in range(len(crc)):
         crc[i].move()
         crc[i].draw(scene)
+
+    for i in range(len(sqe)):
+        sqe[i].move()
+        sqe[i].draw(scene)
 
     # ============================================
     pygame.display.flip()
